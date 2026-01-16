c52716ddfb33ee4a89326a9ad852ee1cfaed2a72
SUDHANSHU RANJAN <code.sudhanshu.ranjan@gmail.com>
2026-01-16 23:44:01 +0530

    changed oops folder

--- 03_OOPS/oops_learn.java
+++ 03_OOPS/oops_learn.java
@@ -1,11 +1,4 @@
 
 public class oops_learn {
-    /*
-    Object-Oriented Programming (OOP) is a programming paradigm that uses "objects" to represent data and methods to manipulate that data. The main principles of OOP are:
-    1. Encapsulation: Bundling the data (attributes) and methods (functions) that operate on the data into a single unit called a class. This helps in restricting direct access to some of an object's components, which can prevent the accidental modification of data.
-    2. Inheritance: A mechanism where a new class (subclass or derived class) can inherit properties and behaviors (attributes and methods) from an existing class (superclass or base class). This promotes code reusability and establishes a hierarchical relationship between classes.
-    3. Polymorphism: The ability of different classes to be treated as instances of the same class through a common interface. It allows methods to do different things based on the object it is acting upon, even though they share the same name. Polymorphism can be achieved through method overriding (runtime polymorphism) and method overloading (compile-time polymorphism).
-    4. Abstraction: The concept of hiding the complex implementation details and showing only the essential features of the object. This helps in reducing complexity and increases efficiency by allowing the programmer to focus on interactions at a higher level.
-    In Java, everything is based on classes and objects. A class is a blueprint for creating objects, and an object is an instance of a class. Java supports all the main principles of OOP, making it a powerful language for building modular and reusable code. 
-     */
+
 }
